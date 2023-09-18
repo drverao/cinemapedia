8ac9074ad8d9c87cf7c46e2fb486579c6d175afb
dverao1997 <diego.r.vera.ortiz@gmail.com>
2023-09-18 03:52:49 -0500

    appbar

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,4 +1,7 @@
+import 'package:cinemapedia/presentation/providers/movies/movies_provider.dart';
+import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class HomeScreen extends StatelessWidget {
   static const name = 'home-screen';
@@ -6,10 +9,44 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Center(
-        child: Placeholder(),
+        child: _HomeView(),
       ),
+    );
+  }
+}
+
+class _HomeView extends ConsumerStatefulWidget {
+  const _HomeView();
+
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends ConsumerState<_HomeView> {
+  @override
+  void initState() {
+    super.initState();
+    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final nowPlayingMoview = ref.watch(nowPlayingMoviesProvider);
+    return Column(
+      children: [
+        const CustomAppbar(),
+        Expanded(
+            child: ListView.builder(
+                itemCount: nowPlayingMoview.length,
+                itemBuilder: (context, index) {
+                  final movie = nowPlayingMoview[index];
+                  return ListTile(
+                    title: Text(movie.title),
+                  );
+                }))
+      ],
     );
   }
 }
