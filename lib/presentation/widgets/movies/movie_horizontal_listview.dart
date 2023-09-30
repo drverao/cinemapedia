d36c60da1c602115ba904e3c3b8d250e8b001f6e
Diego Vera <diego.r.vera.ortiz@gmail.com>
2023-09-30 03:48:01 -0500

    Crear dominio de movie Db

--- lib/presentation/widgets/movies/movie_horizontal_listview.dart
+++ lib/presentation/widgets/movies/movie_horizontal_listview.dart
@@ -2,6 +2,7 @@ import 'package:animate_do/animate_do.dart';
 import 'package:cinemapedia/config/helpers/human_formats.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class MovieHorizontalListView extends StatefulWidget {
   final List<Movie> movies;
@@ -89,10 +90,13 @@ class _Slide extends StatelessWidget {
                       fit: BoxFit.cover, width: 150,
                       loadingBuilder: (context, child, loadingProgress) {
                     if (loadingProgress != null) {
-                      return const Center(
+                      return const Padding(
+                          padding: EdgeInsets.all(8.0),
                           child: CircularProgressIndicator(strokeWidth: .2));
                     }
-                    return FadeIn(child: child);
+                    return GestureDetector(
+                        onTap: () => context.push('/movie/${movie.id}'),
+                        child: FadeIn(child: child));
                   }))),
           //* Title
           SizedBox(
