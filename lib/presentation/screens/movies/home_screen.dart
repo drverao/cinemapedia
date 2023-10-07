a55fa96909ecc3559ce828e7d9b90c08af1c5d09
Diego Vera <diego.r.vera.ortiz@gmail.com>
2023-10-07 02:59:21 -0500

    fin seccion

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,97 +1,24 @@
+import 'package:cinemapedia/presentation/views/views.dart';
 import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-import '../../providers/providers.dart';
 
 class HomeScreen extends StatelessWidget {
   static const name = 'home-screen';
-  const HomeScreen({super.key});
+  final int pageIndex;
+  const HomeScreen({super.key, required this.pageIndex});
+
+  final viewRoutes = const <Widget>[HomeView(), SizedBox(), FavoritesView()];
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: _HomeView(),
-      bottomNavigationBar: CustomBottomNavigationbar(),
+    return Scaffold(
+      body: IndexedStack(
+        index: pageIndex,
+        children: viewRoutes,
+      ),
+      bottomNavigationBar: CustomBottomNavigationbar(
+        currentIndex: pageIndex,
+      ),
     );
-  }
-}
-
-class _HomeView extends ConsumerStatefulWidget {
-  const _HomeView();
-
-  @override
-  _HomeViewState createState() => _HomeViewState();
-}
-
-class _HomeViewState extends ConsumerState<_HomeView> {
-  @override
-  void initState() {
-    super.initState();
-    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
-    ref.read(popularMoviesProvider.notifier).loadNextPage();
-    ref.read(topRatedMoviesProvider.notifier).loadNextPage();
-    ref.read(upcomingMoviesProvider.notifier).loadNextPage();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final initialLoading = ref.watch(initialLoadingProvider);
-
-    if (initialLoading) return const FullScreenLoader();
-
-    final nowPlayingMoview = ref.watch(nowPlayingMoviesProvider);
-    final slideShowMovies = ref.watch(moviesSlideshowProvider);
-    final popularMovies = ref.watch(popularMoviesProvider);
-    final topRatedMovies = ref.watch(topRatedMoviesProvider);
-    final upcomingMovies = ref.watch(upcomingMoviesProvider);
-
-    return CustomScrollView(slivers: [
-      const SliverAppBar(
-          floating: true,
-          flexibleSpace: FlexibleSpaceBar(
-            title: CustomAppbar(),
-            titlePadding: EdgeInsets.zero,
-          )),
-      SliverList(
-          delegate: SliverChildBuilderDelegate((context, index) {
-        return Column(
-          children: [
-            MoviesSlideshow(movies: slideShowMovies),
-            MovieHorizontalListView(
-                movies: nowPlayingMoview,
-                title: 'En cines',
-                subtitle: 'Lunes 20',
-                loadNextPage: () {
-                  ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
-                }),
-            MovieHorizontalListView(
-                movies: upcomingMovies,
-                title: 'Proximamente',
-                subtitle: 'En este mes',
-                loadNextPage: () {
-                  ref.read(upcomingMoviesProvider.notifier).loadNextPage();
-                }),
-            MovieHorizontalListView(
-                movies: popularMovies,
-                title: 'Populares',
-                // subtitle: 'Eneste mes',
-                loadNextPage: () {
-                  ref.read(popularMoviesProvider.notifier).loadNextPage();
-                }),
-            MovieHorizontalListView(
-                movies: topRatedMovies,
-                title: 'Mejor calificadas',
-                subtitle: 'Desde Siempre',
-                loadNextPage: () {
-                  ref.read(topRatedMoviesProvider.notifier).loadNextPage();
-                }),
-            const SizedBox(
-              height: 10,
-            )
-          ],
-        );
-      }, childCount: 1))
-    ]);
   }
 }
--- lib/presentation/widgets/shared/custom_bottom_navigationbar.dart
+++ lib/presentation/widgets/shared/custom_bottom_navigationbar.dart
@@ -1,16 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomBottomNavigationbar extends StatelessWidget {
-  const CustomBottomNavigationbar({super.key});
+  final int currentIndex;
+  const CustomBottomNavigationbar({super.key, required this.currentIndex});
+
+  void onItemTapped(BuildContext context, int index) {
+    switch (index) {
+      case 0:
+        context.go('/home/0');
+        break;
+      case 1:
+        context.go('/home/1');
+        break;
+      case 2:
+        context.go('/home/2');
+        break;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(elevation: 0, items: const [
-      BottomNavigationBarItem(icon: Icon(Icons.home_max), label: 'Home'),
-      BottomNavigationBarItem(
-          icon: Icon(Icons.label_outline), label: 'Categories'),
-      BottomNavigationBarItem(
-          icon: Icon(Icons.favorite_outlined), label: 'Favorites')
-    ]);
+    return BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: (value) => onItemTapped(context, value),
+        elevation: 0,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home_max), label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.label_outline), label: 'Categories'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.favorite_outlined), label: 'Favorites')
+        ]);
   }
 }
